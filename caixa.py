b942e67dac9d5353ef666c27afb202fac05714fd
Weverton Santos <72475645+wevertonsantos@users.noreply.github.com>
2025-07-07 13:53:14 -0300

    criando dicionário com as contas

--- caixa.py
+++ caixa.py
@@ -13,10 +13,13 @@ Encerre a sessão.
 '''
 
 def caixa_eletronico():
-    conta = 12345 #input("Digite sua conta: ")
-    senha = "12345" #input("Digite sua senha: ")
+    conta = 1234 #input("Digite sua conta de 4 digitos: ")
+    senha = 1234 #input("Digite sua senha de 4 digitos: ")
 
-    lista_contas = [1234,12345,3214,2345]
+    lista_contas = [1234,5432,3214,2345]
+    contas = {
+        1234: {"usuário": "joão","senha":"1234","saldo": 1000.00}
+    }
     if conta in lista_contas:
         ...
     else:
