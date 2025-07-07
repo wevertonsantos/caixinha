423343436082e918cf9aaf1bb5252d8fc944a58e
Weverton Santos <72475645+wevertonsantos@users.noreply.github.com>
2025-07-07 14:18:07 -0300

    verificação se a conta do usuário está em contas

--- caixa.py
+++ caixa.py
@@ -20,19 +20,25 @@ def caixa_eletronico():
         1234: {"usuário": "joão","senha":"1234","saldo": 1000.00},
         1235: {"usuário": "joão","senha":"1235","saldo": 1000.00}
     }
-
-    # verificação se a conta do usuário está em contas
-    if conta_usuario in contas:
+    
+    if verificao_conta(conta_usuario,contas):
         # para cada conta
         for conta in contas:
             # verificação se a conta do usuário é a mesma conta
             if conta_usuario == conta:
-                # verifcação senha do usuário igual a senha da conta
+                # verificação senha do usuário igual a senha da conta
                 if contas[conta]['senha'] == senha_usuario:
-                    print("Entrou na conta com sucesso!")
+                    return True
                 else:
-                    print("Senha errada.")
+                    return False
     else:
         print("Essa conta não existe")
+        
+def verificao_conta(conta_usuario,contas):
+    # verificação se a conta do usuário está em contas
+    if conta_usuario in contas:
+        return True
+    else:
+        return False
 
 caixa_eletronico()
