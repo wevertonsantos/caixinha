33207be5f45134a6a0692e95fc7d4639e5c52cd6
Weverton Santos <72475645+wevertonsantos@users.noreply.github.com>
2025-07-07 14:00:46 -0300

    verificação se a conta do usuário é a mesma conta

--- caixa.py
+++ caixa.py
@@ -13,15 +13,21 @@ Encerre a sessão.
 '''
 
 def caixa_eletronico():
-    conta = 1234 #input("Digite sua conta de 4 digitos: ")
-    senha = 1234 #input("Digite sua senha de 4 digitos: ")
+    conta_usuario = 1235 #input("Digite sua conta de 4 digitos: ")
+    senha_usuario = 1333 #input("Digite sua senha de 4 digitos: ")
 
-    lista_contas = [1234,5432,3214,2345]
     contas = {
-        1234: {"usuário": "joão","senha":"1234","saldo": 1000.00}
+        1234: {"usuário": "joão","senha":"1234","saldo": 1000.00},
+        1235: {"usuário": "joão","senha":"1235","saldo": 1000.00}
     }
-    if conta in lista_contas:
-        ...
+
+    # verificação se a conta do usuário está em contas
+    if conta_usuario in contas:
+        # para cada conta
+        for conta in contas:
+            # verificação se a conta do usuário é a mesma conta
+            if conta_usuario == conta:
+                print(contas[conta]['senha'])
     else:
         print("Essa conta não existe")
 
